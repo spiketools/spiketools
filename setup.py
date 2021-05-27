238b1715d40c1448ab78486cce969cb1976fdfea
Tom Donoghue <tdonoghue.research@gmail.com>
2021-05-27 16:15:57 -0400

    update / fix setup

--- setup.py
+++ setup.py
@@ -4,27 +4,27 @@ import os
 from setuptools import setup, find_packages
 
 # Get the current version number from inside the module
-with open(os.path.join('spike_tools', 'version.py')) as version_file:
+with open(os.path.join('spiketools', 'version.py')) as version_file:
     exec(version_file.read())
 
 # Load the long description from the README
-#with open('README.rst') as readme_file:
-#    long_description = readme_file.read()
+with open('README.rst') as readme_file:
+    long_description = readme_file.read()
 
 # Load the required dependencies from the requirements file
 with open("requirements.txt") as requirements_file:
     install_requires = requirements_file.read().splitlines()
 
 setup(
-    name = 'spike_tools',
+    name = 'spiketools',
     version = __version__,
-    description = 'SpikeTools',
-    #long_description = long_description,
+    description = 'Module for analyzing spiking data.',
+    long_description = long_description,
     python_requires = '>=3.6',
     packages = find_packages(),
     license = 'Apache License, 2.0',
     classifiers = [
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 3 - Alpha'
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering',
         'License :: OSI Approved :: Apache Software License',
@@ -40,7 +40,7 @@ setup(
     ],
     platforms = 'any',
     project_urls = {},
-    #download_url = 'https://github.com/XX/XX/releases',
+    download_url = 'https://github.com/TomDonoghue/SpikeTools//releases',
     keywords = ['neuroscience', 'single units', 'spike analyses', 'electrophysiology'],
     install_requires = install_requires,
     tests_require = ['pytest'],
