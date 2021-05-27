521c63b091373067e038a00977a5e5ed1b0ea68c
Tom Donoghue <tdonoghue.research@gmail.com>
2021-05-27 15:49:38 -0400

    add initial setup file

---
+++ setup.py
@@ -0,0 +1,47 @@
+"""SpikeTools setup script."""
+
+import os
+from setuptools import setup, find_packages
+
+# Get the current version number from inside the module
+with open(os.path.join('spike_tools', 'version.py')) as version_file:
+    exec(version_file.read())
+
+# Load the long description from the README
+#with open('README.rst') as readme_file:
+#    long_description = readme_file.read()
+
+# Load the required dependencies from the requirements file
+with open("requirements.txt") as requirements_file:
+    install_requires = requirements_file.read().splitlines()
+
+setup(
+    name = 'spike_tools',
+    version = __version__,
+    description = 'SpikeTools',
+    #long_description = long_description,
+    python_requires = '>=3.6',
+    packages = find_packages(),
+    license = 'Apache License, 2.0',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+    platforms = 'any',
+    project_urls = {},
+    #download_url = 'https://github.com/XX/XX/releases',
+    keywords = ['neuroscience', 'single units', 'spike analyses', 'electrophysiology'],
+    install_requires = install_requires,
+    tests_require = ['pytest'],
+)
