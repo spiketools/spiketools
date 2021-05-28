539f07943e258cac61572ec684a5bd4778d07fdc
Tom Donoghue <tdonoghue.research@gmail.com>
2021-05-27 23:23:18 -0400

    add permutations code

---
+++ spiketools/stats/permutations.py
@@ -0,0 +1,35 @@
+"""   """
+
+import numpy as np
+
+###################################################################################################
+###################################################################################################
+
+def vec_perm(data, n_perms=1000):
+    """Vectorized permutations.
+
+    Parameters
+    ----------
+    data :
+        xx
+    n_perms : int
+        Number of permutations to do.
+
+    Returns
+    -------
+    perms :
+        xx
+
+    Notes
+    -----
+    Code adapted from here:
+    https://stackoverflow.com/questions/46859304/
+    """
+
+    data_ext = np.concatenate((data, data[:-1]))
+    strides = data.strides[0]
+    perms = np.lib.stride_tricks.as_strided(data_ext, shape=(n_perms, len(data)),
+                                            strides=(strides, strides),
+                                            writeable=False).copy()
+
+    return perms
