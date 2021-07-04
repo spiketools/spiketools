fc4b3215a7cb64bc7571c5d67b517fc9980af1b1
Tom Donoghue <tdonoghue.research@gmail.com>
2021-07-04 14:28:07 -0400

    update test procedures for plots

--- spiketools/tests/conftest.py
+++ spiketools/tests/conftest.py
@@ -1,19 +1,37 @@
 """Pytest configuration file for testing spiketools."""
 
+import os
+import shutil
 import pytest
 
 import numpy as np
 
 from spiketools.objects import Cell
 from spiketools.utils import set_random_seed
+from spiketools.tests.tsettings import BASE_TEST_FILE_PATH, TEST_PLOTS_PATH
 
 ###################################################################################################
 ###################################################################################################
+
+## TEST SETUP
 
 def pytest_configure(config):
 
     set_random_seed(42)
 
+@pytest.fixture(scope='session', autouse=True)
+def check_dir():
+    """Once, prior to session, this will clear and re-initialize the test file directories."""
+
+    # If the directories already exist, clear them
+    if os.path.exists(BASE_TEST_FILE_PATH):
+        shutil.rmtree(BASE_TEST_FILE_PATH)
+
+    # Remake (empty) directories
+    os.mkdir(BASE_TEST_FILE_PATH)
+    os.mkdir(TEST_PLOTS_PATH)
+
+## TEST OBJECTS
 
 @pytest.fixture(scope='session')
 def tspikes():
