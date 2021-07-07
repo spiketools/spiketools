37d54be48a2a3e441f73f6420217c4ef1b72035a
Tom Donoghue <tdonoghue.research@gmail.com>
2021-07-07 16:11:23 -0400

    add position & space measures

---
+++ spiketools/spatial/position.py
@@ -0,0 +1,84 @@
+"""Position related functions."""
+
+import numpy as np
+
+###################################################################################################
+###################################################################################################
+
+def compute_distance(x1, y1, x2, y2):
+    """Compute the distance between two positions.
+
+    Parameters
+    ----------
+    x1, y1 : float
+        The X & Y values for the first position.
+    x2, y2 : float
+        The X & Y values for the second position.
+
+    Returns
+    -------
+    float
+        Distance between the two positions.
+    """
+
+    return np.linalg.norm(np.array([x1, y1]) - np.array([x2, y2]))
+
+
+def compute_distances(xs, ys):
+    """Compute distances across a sequence of positions.
+
+    Parameters
+    ----------
+    xs, ys : 1d array
+        Position data, of X and Y locations.
+
+    Returns
+    -------
+    1d array
+        Vector of distances between positions.
+    """
+
+    dists = np.zeros(len(xs) - 1)
+    for ix, (xi, yi, xj, yj) in enumerate(zip(xs, ys, xs[1:], ys[1:])):
+        dists[ix] = compute_distance(xi, yi, xj, yj)
+
+    return dists
+
+
+def compute_cumulative_distances(xs, ys):
+    """Compute cumulative distance across a sequence of positions.
+
+    Parameters
+    ----------
+    xs, ys : 1d array
+        Position data, of X and Y locations.
+
+    Returns
+    -------
+    1d array
+        Cumulative distances.
+    """
+
+    return np.cumsum(compute_distances(xs, ys))
+
+
+def compute_speed(xs, ys, bin_widths):
+    """Compute speeds across a sequence of positions.
+
+    Parameters
+    ----------
+    xs, ys : 1d array
+        Position data, of X and Y locations.
+    bin_widths : 1d array
+        Width of each position bin.
+
+    Returns
+    -------
+    speed : 1d array
+        Vector of speeds across each position step.
+    """
+
+    distances = compute_distances(xs, ys)
+    speed = distances / bin_widths
+
+    return speed
