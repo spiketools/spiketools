75d8e6927440f7b990c2a4f6371e2479fd7f1af7
maestapereira <sm5150@columbia.edu>
2021-09-29 14:16:38 -0400

    Added test function to test_compute_spatial_bin_edges

--- spiketools/tests/spatial/test_occupancy.py
+++ spiketools/tests/spatial/test_occupancy.py
@@ -1,12 +1,24 @@
 """Tests for spiketools.spatial.occupancy"""
 
 from spiketools.spatial.occupancy import *
+import numpy as np
 
 ###################################################################################################
 ###################################################################################################
 
-def test_compute_spatial_bin_edges():
-    pass
+def test_compute_spatial_bin_edges(position, bins):
+    x_edges, y_edges = compute_spatial_bin_edges(position, bins)
+	# dimension checks
+    assert len(x_edges) == bins[0]+1
+    assert len(y_edges) == bins[1]+1
+	# first and last element checks
+    assert x_edges[0] == min(position[0, :])
+    assert x_edges[-1] == max(position[0, :])
+    assert y_edges[0] == min(position[1, :])
+    assert y_edges[-1] == max(position[1, :])
+	# sorting check
+    assert bool((np.sort(x_edges) == x_edges).sum())
+    assert bool((np.sort(y_edges) == y_edges).sum())
 
 def test_compute_spatial_bin_assignment():
     pass
