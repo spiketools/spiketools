0d0df6cbfec4ce3d6e43268d68e197956a8024ec
maestapereira <sm5150@columbia.edu>
2021-10-05 11:04:31 -0400

    Modified test_compute_spatial_bin_edges within test_occupancy.py. Removed inputs, added some extra test cases.

--- spiketools/tests/spatial/test_occupancy.py
+++ spiketools/tests/spatial/test_occupancy.py
@@ -6,19 +6,31 @@ import numpy as np
 ###################################################################################################
 ###################################################################################################
 
-def test_compute_spatial_bin_edges(position, bins):
+def test_compute_spatial_bin_edges():
+    # test for regular input (x) and all zeros input (y)
+    bins = [4, 4]
+    position = np.zeros((2, 4))
+    position[0, :] = np.linspace(1, 4, 4)
     x_edges, y_edges = compute_spatial_bin_edges(position, bins)
-	# dimension checks
+    # dimension checks
     assert len(x_edges) == bins[0]+1
     assert len(y_edges) == bins[1]+1
-	# first and last element checks
+    # all zeros case check
+    assert np.sum(y_edges == np.linspace(-0.5, 0.5, bins[1]+1)) == bins[1]+1
+    # first and last element checks for non all-zero input
     assert x_edges[0] == min(position[0, :])
     assert x_edges[-1] == max(position[0, :])
-    assert y_edges[0] == min(position[1, :])
-    assert y_edges[-1] == max(position[1, :])
-	# sorting check
+    # sorting check
     assert bool((np.sort(x_edges) == x_edges).sum())
     assert bool((np.sort(y_edges) == y_edges).sum())
+    
+    # now check for one input the suffle of the other
+    bins = [4, 4]
+    position2 = position
+    position[1, :] = position[0, :]
+    np.random.shuffle(position2[0, :])
+    x_edges, y_edges = compute_spatial_bin_edges(position2, bins)
+    assert np.sum(x_edges2 == y_edges2) == bins[0]+1
 
 def test_compute_spatial_bin_assignment():
     pass
