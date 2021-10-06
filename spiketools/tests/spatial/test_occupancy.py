d0a6609e5947b5f39db38dc746668eb8b358a567
maestapereira <sm5150@columbia.edu>
2021-10-06 13:56:21 -0400

    Changed order of test cases in test_compute_spatial_bin_edges(): from simpler to more unnusual test cases.

--- spiketools/tests/spatial/test_occupancy.py
+++ spiketools/tests/spatial/test_occupancy.py
@@ -7,30 +7,34 @@ import numpy as np
 ###################################################################################################
 
 def test_compute_spatial_bin_edges():
-    # test for regular input (x) and all zeros input (y)
-    bins = [4, 4]
-    position = np.zeros((2, 4))
-    position[0, :] = np.linspace(1, 4, 4)
+    # checks for two inputs of the same size, different number of bins
+    position = np.array([[1., 2., 3., 4.], [0., 1., 2., 3.]])
+    bins = [4, 3]
     x_edges, y_edges = compute_spatial_bin_edges(position, bins)
     # dimension checks
     assert len(x_edges) == bins[0]+1
     assert len(y_edges) == bins[1]+1
-    # all zeros case check
-    assert np.sum(y_edges == np.linspace(-0.5, 0.5, bins[1]+1)) == bins[1]+1
-    # first and last element checks for non all-zero input
+    # first and last element checks
     assert x_edges[0] == min(position[0, :])
     assert x_edges[-1] == max(position[0, :])
+    assert y_edges[0] == min(position[1, :])
+    assert y_edges[-1] == max(position[1, :])
     # sorting check
     assert bool((np.sort(x_edges) == x_edges).sum())
     assert bool((np.sort(y_edges) == y_edges).sum())
     
-    # now check for one input the suffle of the other
+    # checks for two inputs such that one is the other one shuffled
+    position = np.array([[1., 2., 3., 4.], [4., 1., 3., 2.]])
     bins = [4, 4]
-    position2 = position
-    position[1, :] = position[0, :]
-    np.random.shuffle(position2[0, :])
-    x_edges2, y_edges2 = compute_spatial_bin_edges(position2, bins)
-    assert np.sum(x_edges2 == y_edges2) == bins[0]+1
+    x_edges, y_edges = compute_spatial_bin_edges(position, bins)
+    # check that bins are the same
+    assert np.sum(x_edges == y_edges) == bins[0]+1
+    
+    # test for regular input (x) and all zeros input (y)
+    position = np.array([[1., 2., 3., 4.], [0., 0., 0., 0.]])
+    x_edges, y_edges = compute_spatial_bin_edges(position, bins)
+    # all zeros case check
+    assert np.sum(y_edges == np.linspace(-0.5, 0.5, bins[1]+1)) == bins[1]+1
 
 def test_compute_spatial_bin_assignment():
     pass
