beab77ae120e12a311cf7db1994c9d5db30d4aa3
claire98han <91143881+claire98han@users.noreply.github.com>
2021-10-07 01:06:24 -0400

    Update occupancy.py

--- spiketools/spatial/occupancy.py
+++ spiketools/spatial/occupancy.py
@@ -6,7 +6,7 @@ import pandas as pd
 ###################################################################################################
 ###################################################################################################
 
-def compute_spatial_bin_edges(position, bins=[x_bins, y_bins]):
+def compute_spatial_bin_edges(position, bins):
     """Compute spatial bin edges.
 
     Parameters
