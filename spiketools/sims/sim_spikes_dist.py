a3f6c4ff1bc255c1046c6e05f796fe56065dae2f
Tom Donoghue <tdonoghue.research@gmail.com>
2021-10-11 17:19:20 -0400

    add poisson func

--- spiketools/sims/sim_spikes_dist.py
+++ spiketools/sims/sim_spikes_dist.py
@@ -22,3 +22,33 @@ def sim_spiketrain_binom(p_spiking, n_samples=None):
     """
 
     return np.random.binomial(1, p=p_spiking, size=n_samples)
+
+
+def sim_spiketrain_poisson(rate, n_samples, fs, bias=0):
+    """Simulate spike train from a Poisson distribution.
+
+    Parameters
+    ----------
+    rate : float
+        The firing rate of neuron to simulate.
+    n_samples : int, optional
+        The number of samples to simulate.
+    fs : int
+        The sampling rate.
+
+    Results
+    -------
+    spikes : 1d array
+        Simulated spike train.
+    """
+
+    spikes = np.zeros(n_samples)
+
+    # Create uniform sampling distribution
+    unif = np.random.uniform(0, 1, size=n_samples)
+
+    # Create spikes
+    mask = unif <= ((rate + bias) * 1/fs)
+    spikes[mask] = 1
+
+    return spikes
