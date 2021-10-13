cfbea664986f132486db3ba8096c0fd0e3d36032
claire98han <91143881+claire98han@users.noreply.github.com>
2021-10-13 19:57:36 -0400

    Update measures.py

--- spiketools/measures/measures.py
+++ spiketools/measures/measures.py
@@ -74,7 +74,7 @@ def compute_cv(isis):
     
     >>> isis = [0.3, 0.6, 0.6, 0.2, 0.7]
     >>> compute_cv(isis)
-   0.4039733214513607
+    0.4039733214513607
     """
 
     return np.std(isis) / np.mean(isis)
