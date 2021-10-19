6ff449912cdc6512c019539ddaf24df55ea0dde1
maestapereira <sm5150@columbia.edu>
2021-10-19 16:04:05 -0400

    Added examples for sim_spiketrain_prob function (sim/prob.py).

--- spiketools/sim/prob.py
+++ spiketools/sim/prob.py
@@ -19,10 +19,24 @@ def sim_spiketrain_prob(p_spiking, n_samples):
     -------
     spikes : 1d array
         Simulated spike train.
-		
+	
     Notes
     -------
     n_samples is only used if p_spiking is a float, otherwise n_samples is just the length of p_spiking.
+
+    Examples
+    -------
+    Simulate spike train of size n_samples, based on a probability of spiking per sample.
+
+	>>> p_spiking = 0.3
+    >>> n_samples = 10
+    >>> sim_spiketrain_prob(p_spiking, n_samples)
+
+    Simulate spike train based on a probability of spiking per sample over time.
+
+	>>> p_spiking = np.array([0.3, 0.5, 0.6])
+    >>> n_samples = len(p_spiking)
+    >>> sim_spiketrain_prob(p_spiking, n_samples)
     """
 
     if isinstance(p_spiking, float):
