3c38169eda39df53b65bb1607244ea2ee7696724
maestapereira <sm5150@columbia.edu>
2021-10-19 15:04:31 -0400

    Fixed inconsistency in variable names in sim_spiketrain_prob (within sim/prob.py) and fixed function.

--- spiketools/sim/prob.py
+++ spiketools/sim/prob.py
@@ -6,25 +6,31 @@ import numpy as np
 ###################################################################################################
 
 def sim_spiketrain_prob(p_spiking, n_samples):
-    """Simulate spikes based on a probability of spiking per sample.
+    """Simulate spikes based on a probability of spiking per unit.
 
     Parameters
     ----------
     p_spiking : float or 1d array
-            The probability (per sample) of spiking.
+        The probability (per unit) of spiking.
     n_samples : int, optional
-        The number of samples to simulate.
+        The number of samples per unit to simulate.
 
     Returns
     -------
-    spikes : 1d array
-        Simulated spike train.
+    spikes : nd array
+        Simulated spike train per unit.
+		
+	Notes
+	-------
+	When p_spiking is an array, each unit corresponds to a row in spikes.
     """
+    if isinstance(p_spiking, float):
+		probs = np.ones(n_samples) * p_spiking
+    else:
+		# for many samples being simulated, each unit corresponds to one row
+		probs = np.ones((len(p_spiking), n_samples))* (p_spiking[:, np.newaxis])
 
-    if isinstance(prob, float):
-        probs = np.ones(length) * prob
-
-    spikes = (probs > np.random.rand(*probs.shape))
-    spikes = spikes.astype(int)
-
-    return spikes
+	spikes = (probs > np.random.rand(*probs.shape))
+	spikes = spikes.astype(int)
+	
+	return spikes
