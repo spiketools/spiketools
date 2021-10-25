2a7459b5cb025b3847123f0aeb7d3db2bf6d85ef
claire98han <91143881+claire98han@users.noreply.github.com>
2021-10-25 12:56:41 -0400

    Update test_utils.py

--- spiketools/tests/sim/test_utils.py
+++ spiketools/tests/sim/test_utils.py
@@ -6,4 +6,15 @@ from spiketools.sim.utils import *
 ###################################################################################################
 
 def test_refractory():
-    pass
+    spikes = np.array([0, 1, 1, 0, 1])
+    refractory_time = 0.002
+    fs = 1000
+    
+    refractory_spikes = refractory(spikes, refractory_time, fs)
+    
+    # output dimension check
+    assert refractory_spikes.shape == spikes.shape
+    
+    # output value check
+    assert refractory_spikes.all == 0 or 1 
+   
