bcd578bdcc5f3a32ae9108d426eb1b89622f2e48
maestapereira <sm5150@columbia.edu>
2021-10-27 10:58:27 -0400

    Added test to sim_spiketrain_binom function (sim/dist.py).

--- spiketools/tests/sim/test_dist.py
+++ spiketools/tests/sim/test_dist.py
@@ -6,7 +6,31 @@ from spiketools.sim.dist import *
 ###################################################################################################
 
 def test_sim_spiketrain_binom():
-    pass
+
+    # Simulate spike train based on a probability of spiking per sample over time.
+    p_spiking_1 = np.ones(10)
+    sim_spiketrain_1 = sim_spiketrain_binom(p_spiking_1)
+    
+    p_spiking_2 = np.array([0.3, 0.5, 0.1, 0.8])
+    sim_spiketrain_2 = sim_spiketrain_binom(p_spiking_2)
+    
+    # Simulate spike train of size n_samples, based on a probability of spiking per sample.
+    p_spiking_3 = 0
+    n_samples = 10
+    sim_spiketrain_3 = sim_spiketrain_binom(p_spiking_3, n_samples)
+    
+    # value checks
+    np.nansum(sim_spiketrain_1) == len(p_spiking_1)
+    np.nansum(sim_spiketrain_3) == 0
+    # dimension checks
+    len(sim_spiketrain_1) == len(p_spiking_1)
+    len(sim_spiketrain_1) == len(p_spiking_2)
+    len(sim_spiketrain_1) == n_samples
+    
+    try:
+        sim_spiketrain_4 = sim_spiketrain_binom(p_spiking_3)
+    except ValueError:
+        pass
 
 def test_sim_spiketrain_poisson():
 
