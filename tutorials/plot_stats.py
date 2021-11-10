8b5858ffe37ab91e76f94e73b01882cef9f5fc8b
maestapereira <sm5150@columbia.edu>
2021-11-10 13:06:01 -0500

    Changed plotting from plt to spiketools functions, style changes.

--- tutorials/plot_stats.py
+++ tutorials/plot_stats.py
@@ -11,9 +11,6 @@ This tutorial primarily covers the ``spiketools.stats`` module.
 # Applying statistical measures to spiking data
 # ---------------------------------------------
 #
-# Sections
-# ~~~~~~~~
-#
 # This tutorial contains the following sections:
 #
 # 1. Compute and plot different shuffles of spikes
@@ -30,10 +27,13 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# import all functions from spiketools.stats
-from spiketools.stats.generators import *
-from spiketools.stats.permutations import *
-from spiketools.stats.shuffle import *
+# import functions from spiketools.stats
+from spiketools.stats.generators import poisson_train
+from spiketools.stats.shuffle import shuffle_isis, shuffle_bins, shuffle_poisson, shuffle_circular
+from spiketools.stats.permutations import compute_empirical_pvalue, zscore_to_surrogates
+
+# import plot_trial_rasters to plot spike trains
+from spiketools.plts.trial import plot_trial_rasters
 
 ###################################################################################################
 
@@ -47,11 +47,15 @@ spikes_ms = np.unique(spikes_ms.astype(int))
 
 ###################################################################################################
 # 1. Compute and plot different shuffles of spikes
-# ~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
-# Use spikes_ms to compute shuffle_isis (with 10 shuffles), shuffle_bins (with 10 shuffles), 
-# shuffle_poisson (with 10 shuffles), and shuffle_circular (with 10 shuffles).
-# Also compute a vector permutation, for comparison.
+# Here, we will explore different shuffle approaches to create different shuffled surrogates of our original spikes. 
+#
+# The approaches we will try are:
+# - `shuffle_isis`: shuffle spike times using permuted inter-spike intervals (isis).
+# - `shuffle_bins`: shuffle spikes by creating bins of varying length and then circularly shuffling within those bins.
+# - `shuffle_poisson`: shuffle spikes based on a Poisson distribution.
+# - `shuffle_circular`: shuffle spikes by circularly shifting the spike train.
 #
 
 ###################################################################################################
@@ -62,58 +66,36 @@ shuffled_bins = shuffle_bins(spikes_ms, bin_width_range=[50, 200], n_shuffles=10
 shuffled_poisson = shuffle_poisson(spikes_ms, n_shuffles=10)
 shuffled_circular = shuffle_circular(spikes_ms, shuffle_min=200, n_shuffles=10)
 
-# Do a vector permutation, for comparison
-perm_spikes = vec_perm(spikes_ms, n_perms=10)
+###################################################################################################
+
+# Plot original spike train
+plot_trial_rasters(spikes_ms[:], xlim=[0, 6000], ylim=[0.5, 1.5], title='Non-shuffled')
 
 ###################################################################################################
 
-# Plot non-shuffled, all different shuffles, and vector permutation
-f, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, sharey = True)
-# original
-ax1.eventplot(spikes_ms[0:50])
-ax1.set_title('Non-shuffled')
-ax1.set_xlim([0, 6000])
-ax1.set_xlabel('time')
+# Plot different shuffles
+f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharey = True)
 
 # isis
-ax2.eventplot(shuffled_isis[:, 0:80])
-ax2.set_title('Shuffle ISIS n_shuffles = 10')
-ax2.set_xlim([0, 6000])
-ax2.set_xlabel('time')
+plot_trial_rasters(shuffled_isis[:, 0:80], xlim=[0, 6000], ax=ax1, title='Shuffle ISIS n_shuffles = 10')
 
 # poisson
-ax3.eventplot(shuffled_poisson[:, :])
-ax3.set_title('Shuffle poisson n_shuffles = 10')
-ax3.set_xlim([0, 6000])
-ax3.set_xlabel('time')
-
-# only plot up to 55th so the effect is visible
-ax4.eventplot(perm_spikes[:, :55])
-ax4.set_title('Permutated spikes n_permutations = 10')
-ax4.set_xlim([0, 6000])
-ax4.set_xlabel('time')
+plot_trial_rasters(shuffled_poisson[:, :], xlim=[0, 6000], ax=ax2, title='Shuffle poisson n_shuffles = 10')
 
 # shuffled bins
-ax5.eventplot(shuffled_bins[:, :])
-ax5.set_title('Shuffle bins n_shuffles = 10')
-ax5.set_xlim([0, 6000])
-ax5.set_xlabel('time')
+plot_trial_rasters(shuffled_bins[:, :], xlim=[0, 6000], ax=ax3, title='Shuffle bins n_shuffles = 10')
 
 # shuffled circular
-ax6.eventplot(shuffled_circular[:, :])
-ax6.set_title('Shuffle circular n_shuffles = 10')
-ax6.set_xlim([0, 6000])
-ax6.set_xlabel('time')
+plot_trial_rasters(shuffled_circular[:, :], xlim=[0, 6000], ax=ax4, title='Shuffle circular n_shuffles = 10')
 
 # Add some padding between subplots
-# f.tight_layout(h_pad = 0.001)
+plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.3)
 # Make figure bigger
 f.set_size_inches((40/2.54, 20/2.54))
-plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.3)
 
 ###################################################################################################
 # 2. Compute the empirical p-value from distribution of surrogates
-# ~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 # Compute the empirical p-value of 100 from the distribution of surrogates (spikes_ms).
 #
@@ -125,7 +107,7 @@ compute_empirical_pvalue(100, spikes_ms)
 
 ###################################################################################################
 # 3. Compute z-score from distribution of surrogates
-# ~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 # Z-score number 100 relative to a distribution of surrogates (spikes_ms).
 #
