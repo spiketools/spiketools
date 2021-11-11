146431559284da8c822467cf5ed184504a0640db
Tom Donoghue <tdonoghue.research@gmail.com>
2021-11-11 14:11:39 -0500

    refactor test_select

--- spiketools/tests/utils/test_select.py
+++ spiketools/tests/utils/test_select.py
@@ -9,25 +9,18 @@ from spiketools.utils.select import *
 
 def test_get_avg_func():
 
-    func = get_avg_func('mean')
-    assert callable(func)
-
-    func = get_avg_func('median')
-    assert callable(func)
+    for avg_type in ['mean', 'median']:
+        func = get_avg_func(avg_type)
+        assert callable(func)
 
     with raises(ValueError):
         get_avg_func('not_a_thing')
 
 def test_get_var_func():
 
-    func = get_var_func('std')
-    assert callable(func)
-
-    func = get_var_func('var')
-    assert callable(func)
-
-    func = get_var_func('sem')
-    assert callable(func)
+    for var_type in ['std', 'var', 'sem']:
+        func = get_var_func(var_type)
+        assert callable(func)
 
     with raises(ValueError):
         get_avg_func('not_a_thing')
