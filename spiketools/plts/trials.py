b792386ff7e4d5367abdce0ddac42b05b4a32fa9
Tom Donoghue <tdonoghue.research@gmail.com>
2021-11-11 15:47:07 -0500

    add plot_binned_spike_rates

--- spiketools/plts/trials.py
+++ spiketools/plts/trials.py
@@ -1,6 +1,10 @@
 """Plots for trials related measures and analyses."""
 
+import numpy as np
+
+from spiketools.plts.annotate import _add_significance_to_plot
 from spiketools.plts.utils import check_ax, savefig, set_plt_kwargs
+from spiketools.utils.select import get_avg_func, get_var_func
 
 ###################################################################################################
 ###################################################################################################
@@ -39,3 +43,59 @@ def plot_trial_rasters(data, line=0, xlim=None, show_axis=False, ax=None, **plt_
 
     if not show_axis:
         ax.set_axis_off()
+
+
+@savefig
+@set_plt_kwargs
+def plot_binned_spike_rates(x_vals, y_vals, average=None, shade=None, labels=None,
+                            stats=None, sig_level=0.05, ax=None, **plt_kwargs):
+    """Plot continuous spike rates.
+
+    Parameters
+    ----------
+    x_vals : 1d array
+        Values for the x-axis, for example time values or bin numbers.
+    y_vals : list of array
+        One or many set of values for the y-axis.
+        If each array is 1d values are plotted directly.
+        If 2d, is to be averaged before plotting.
+    average : {'mean', 'median'}, optional
+        Averaging to apply to firing rate activity before plotting.
+    shade : {'sem', 'std'} or list of array, optional
+        Measure of variance to compute and/or plot as shading.
+    labels : list of str, optional
+        Labels for each set of y-values.
+        If provided, a legend is added to the plot.
+    stats : list, optional
+        Statistical results, including p-values, to use to annotate the plot.
+    sig_level : float, optional, default: 0.05
+        Threshold level to consider a result significant.
+    ax : Axes, optional
+        Axis object upon which to plot.
+    plt_kwargs
+        Additional arguments to pass into the plot function.
+    """
+
+    ax = check_ax(ax, figsize=plt_kwargs.pop('figsize', None))
+
+    if not isinstance(y_vals[0], np.ndarray):
+        y_vals = [y_vals]
+
+    if isinstance(shade, str):
+        shade = [get_var_func(shade)(arr, 0) for arr in y_vals]
+
+    if isinstance(average, str):
+        y_vals = [get_avg_func(average)(arr, 0) for arr in y_vals]
+
+    for ind, ys in enumerate(y_vals):
+
+        ax.plot(x_vals, ys, lw=3, label=labels[ind] if labels else None, **plt_kwargs)
+
+        if shade:
+            ax.fill_between(x_vals, ys-shade[ind], ys+shade[ind], alpha=0.25)
+
+    if labels:
+        ax.legend(loc='best')
+
+    if stats:
+        _add_significance_to_plot(stats, sig_level=sig_level, ax=ax)
--- spiketools/tests/plts/test_trials.py
+++ spiketools/tests/plts/test_trials.py
@@ -19,3 +19,18 @@ def test_plot_trial_rasters():
 
     plot_trial_rasters(data,
                        file_path=TEST_PLOTS_PATH, file_name='test_plot_trial_rasters.png')
+
+
+@plot_test
+def test_plot_binned_spike_rates():
+
+    x_vals = np.array([1, 2, 3, 4, 5])
+    y_vals1 = np.array([[2, 2, 2, 2, 2], [3, 3, 3, 3, 3], [4, 4, 4, 4, 4]])
+    y_vals2 = np.array([[3, 3, 3, 3, 3], [4, 4, 4, 4, 4], [5, 5, 5, 5, 5]])
+
+    plot_binned_spike_rates(x_vals, np.mean(y_vals1, 0),
+                            file_path=TEST_PLOTS_PATH, file_name='test_plot_binned_spike_rates1.png')
+
+    plot_binned_spike_rates(x_vals, [y_vals1, y_vals2], average='median', shade='sem',
+                            labels=['A', 'B'], stats=[0.5, 0.01, 0.5, 0.01, 0.5],
+                            file_path=TEST_PLOTS_PATH, file_name='test_plot_binned_spike_rates2.png')
