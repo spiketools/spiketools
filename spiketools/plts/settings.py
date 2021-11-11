5b23cea9326db0c962ced1521dd364e440b40433
Tom Donoghue <tdonoghue.research@gmail.com>
2021-11-11 13:13:15 -0500

    add plt settings file

---
+++ spiketools/plts/settings.py
@@ -0,0 +1,7 @@
+"""Default settings for plots."""
+
+###################################################################################################
+###################################################################################################
+
+# Define a list of arguments that will be caught and applied with 'set'
+SET_KWARGS = ['title', 'xlim', 'ylim', 'xlabel', 'ylabel']
