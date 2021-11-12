1559a6f2a52cef28be2ff6570b7d8eb225f32a5d
Tom Donoghue <tdonoghue.research@gmail.com>
2021-11-11 23:07:57 -0500

    fix up plot_rasters

--- spiketools/plts/trials.py
+++ spiketools/plts/trials.py
@@ -13,19 +13,21 @@ from spiketools.utils.base import flatten
 
 @savefig
 @set_plt_kwargs
-def plot_rasters(data, line=0, show_axis=False, colors=None, ax=None, **plt_kwargs):
+def plot_rasters(data, line=0, colors=None, show_axis=False, ax=None, **plt_kwargs):
     """Plot rasters across multiple trials.
 
     Parameters
     ----------
-    data : list of list
+    data : list of list of float
         Spike times per trial.
+        Multiple conditions can also be passed in.
     line : float, optional, default: 0
         Position to draw a vertical line. If None, no line is drawn.
+    colors : str or list of str
+        Color(s) to plot the raster ticks.
+        If more than one, should be the length of data.
     show_axis : bool, optional, default: False
         Whether to show the axis around the plot.
-    colors : str or list of str
-        xx
     ax : Axes, optional
         Axis object upon which to plot.
     plt_kwargs
@@ -34,7 +36,15 @@ def plot_rasters(data, line=0, show_axis=False, colors=None, ax=None, **plt_kwar
 
     ax = check_ax(ax, figsize=plt_kwargs.pop('figsize', None))
 
-    if isinstance(data[0][0], list):
+    while True:
+        ind = 0
+        try:
+            checker = data[ind][0]
+            break
+        except IndexError:
+            ind += 1
+
+    if isinstance(checker, list):
         lens = [len(el) for el in data]
         colors = DEFAULT_COLORS[0:len(lens)] if not colors else colors
         colors = flatten([[col] * ll for col, ll in zip(colors, lens)])
@@ -43,7 +53,9 @@ def plot_rasters(data, line=0, show_axis=False, colors=None, ax=None, **plt_kwar
     ax.eventplot(data, colors=colors)
 
     if line is not None:
-        ax.vlines(line, -1, len(data), lw=2.5, color='green', alpha=0.5)
+        line = [line] if isinstance(line, (int, float)) else line
+        for ll in line:
+            ax.vlines(ll, -1, len(data), lw=2.5, color='green', alpha=0.5)
 
     if not show_axis:
         ax.set_axis_off()
