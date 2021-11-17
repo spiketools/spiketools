bdd1747009d4d7b014463e6c3a61f66732c5a42b
Tom Donoghue <tdonoghue.research@gmail.com>
2021-11-17 14:31:08 -0500

    add make_axes

--- spiketools/tests/plts/test_utils.py
+++ spiketools/tests/plts/test_utils.py
@@ -2,6 +2,7 @@
 
 import os
 
+from spiketools.tests.tutils import plot_test
 from spiketools.tests.tsettings import TEST_PLOTS_PATH
 
 from spiketools.plts.utils import *
@@ -64,3 +65,13 @@ def test_set_plt_kwargs():
 
     example_plot(title=title)
     assert plt.gca().get_title() == title
+
+@plot_test
+def test_make_axes():
+
+    n_axes = 5
+
+    axes = make_axes(n_axes)
+    assert len(axes) == n_axes
+
+    make_axes(2, 2, row_size=2, col_size=2, wspace=0.1, hspace=0.1)
