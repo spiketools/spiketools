8fdaafcf1c2b8213b96fbbcbeae50e28303a3cb4
Tom Donoghue <tdonoghue.research@gmail.com>
2021-11-18 11:57:01 -0500

    fix up cell delimiters

--- tutorials/plot_measures.py
+++ tutorials/plot_measures.py
@@ -35,14 +35,16 @@ from spiketools.plts.spikes import plot_isis
 from spiketools.plts.utils import check_ax, savefig
 
 ###################################################################################################
-
+#
 # Compute measures of spiking activity
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-# 
+#
+#
 # First, we estimate spike rate from a vector of spike times, in seconds.
-
-# Here, spike time refers to a representation of spiking activity based on listing the times at which spikes occur and spike rate measures how fast an individual neuron is firing.
+#
+# Here, spike time refers to a representation of spiking activity based on listing the times at
+# which spikes occur and spike rate measures how fast an individual neuron is firing.
+#
 
 ###################################################################################################
 
@@ -55,20 +57,21 @@ plt.eventplot(spikes)
 # compute its spike rate
 print('The spike rate is', compute_spike_rate(spikes))
 
-######## (continuing)
-
-# Then, we can compute the inter-spike intervals, measures of the time intervals between successive spikes, of that vector of spike times.
+###################################################################################################
+#
+# Then, we can compute the inter-spike intervals, measures of the time intervals between
+# successive spikes, of that vector of spike times.
 #
 
 ###################################################################################################
 
 isis = compute_isis(spikes)
 
-# plot the inter-spike intervals 
+# plot the inter-spike intervals
 plot_isis(isis, bins=None, range=None, density=False, ax=None)
 
-######## (continuing)
-
+###################################################################################################
+#
 # Next, we can further compute the coefficient of variation of interval-spike intervals we just calculated.
 #
 
@@ -77,28 +80,29 @@ plot_isis(isis, bins=None, range=None, density=False, ax=None)
 cv = compute_cv(isis)
 print('Coefficient of variation:', cv)
 
-######## (continuing)
-
+###################################################################################################
+#
 # Finally, we can compute the fano factor, which is a measure of the variability of unit firing, of a spike train.
 #
 
 ###################################################################################################
 
-# set a spike train 
+# set a spike train
 spike_train = [0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0]
 
-# Compute the fano factor 
+# Compute the fano factor
 fano = compute_fano_factor(spike_train)
 
 # Print out the computed fano factor
 print('Fano factor: {:1.2f}'.format(fano))
 
 ###################################################################################################
-
+#
 # Convert spiking data
 # ~~~~~~~~~~~~~~~~~~~~
-
-# First, we convert a vector of spike times in millisecond to a binary spike train, which is a representation of spiking activity in which spikes are. 
+#
+# First, we convert a vector of spike times in millisecond to a binary spike train, which is
+# a representation of spiking activity in which spikes are.
 #
 
 ###################################################################################################
@@ -110,10 +114,10 @@ spikes = [1, 4, 5, 6, 9, 16, 17, 20, 25, 28, 40]
 spike_train = create_spike_train(spikes)
 print('Spike train:', spike_train)
 
-######## (continuing)
-
+###################################################################################################
+#
 # Similarly, we can also convert binary spike train to spike times in millisecond.
-# 
+#
 
 ###################################################################################################
 
@@ -126,10 +130,10 @@ spike_times = convert_train_to_times(spike_train)
 print('Spike times:', spike_times)
 plt.eventplot(spike_times)
 
-######## (continuing)
-
+###################################################################################################
+#
 # Finally, we can convert a vector of inter-spike intervals in millisecond to spike times in millisecond.
-# 
+#
 
 ###################################################################################################
 
