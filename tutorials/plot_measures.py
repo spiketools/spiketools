48c3eba34b32a858416688df1af9a7ab3cfc99e0
claire98han <zh2497@columbia.edu>
2021-11-25 23:34:19 -0500

    updated plots

--- tutorials/plot_measures.py
+++ tutorials/plot_measures.py
@@ -32,30 +32,39 @@ import matplotlib.pyplot as plt
 from spiketools.measures.measures import compute_spike_rate, compute_isis, compute_cv, compute_fano_factor
 from spiketools.measures.conversions import create_spike_train, convert_train_to_times, convert_isis_to_spikes
 from spiketools.plts.spikes import plot_isis
-from spiketools.plts.utils import check_ax, savefig
+from spiketools.sim.prob import sim_spiketrain_prob
+from spiketools.plts.trials import plot_rasters
 
 ###################################################################################################
 #
 # Compute measures of spiking activity
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
-#
-# First, we estimate spike rate from a vector of spike times, in seconds.
+# First, we estimate spike rate from a vector of spike times, in milliseconds.
 #
 # Here, spike time refers to a representation of spiking activity based on listing the times at
-# which spikes occur and spike rate measures how fast an individual neuron is firing.
+# which spikes occur and spike rate measures how fast an individual neuron is firing. Examples of spike
+# train and spike times are provided below.
 #
 
 ###################################################################################################
 
-# set and visualize a series of spike times
-spikes = [0.1, 0.3, 0.9, 1.9, 2.0, 2.1, 2.2, 3.4, 3.5, 3.7, 4.6, 4.8, 4.9, 7]
-plt.eventplot(spikes)
+# generate a binary spike train and its corresponding spike times in milliseconds
+p_spiking = np.random.random(100)
+spike_train = sim_spiketrain_prob(p_spiking)
+spike_times = convert_train_to_times(spike_train)
+
+# print the first 20 spikes in the binary spike train
+print(spike_train[:20])
+
+# plot the spike times
+plot_rasters(spike_times)
 
 ###################################################################################################
 
-# compute its spike rate
-print('The spike rate is', compute_spike_rate(spikes))
+# compute the spike rate given a vector of spike times in milliseconds
+spike_rate = compute_spike_rate(spike_times)
+print('The spike rate is', spike_rate)
 
 ###################################################################################################
 #
@@ -65,7 +74,8 @@ print('The spike rate is', compute_spike_rate(spikes))
 
 ###################################################################################################
 
-isis = compute_isis(spikes)
+# compute the interval-spike intervals of a vector of spike times in milliseconds
+isis = compute_isis(spike_times)
 
 # plot the inter-spike intervals
 plot_isis(isis, bins=None, range=None, density=False, ax=None)
@@ -87,13 +97,8 @@ print('Coefficient of variation:', cv)
 
 ###################################################################################################
 
-# set a spike train
-spike_train = [0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0]
-
-# Compute the fano factor
+# Compute the fano factor of a binary spike train
 fano = compute_fano_factor(spike_train)
-
-# Print out the computed fano factor
 print('Fano factor: {:1.2f}'.format(fano))
 
 ###################################################################################################
@@ -101,47 +106,42 @@ print('Fano factor: {:1.2f}'.format(fano))
 # Convert spiking data
 # ~~~~~~~~~~~~~~~~~~~~
 #
-# First, we convert a vector of spike times in millisecond to a binary spike train, which is
+# First, we convert a vector of spike times in milliseconds to a binary spike train, which is
 # a representation of spiking activity in which spikes are.
 #
 
 ###################################################################################################
 
-# set a vector of spike times in milliseconds
-spikes = [1, 4, 5, 6, 9, 16, 17, 20, 25, 28, 40]
+# convert a vector of spike times in milliseconds to a binary spike train
+spike_train = create_spike_train(spike_times)
 
-# convert spike times to binary spike train
-spike_train = create_spike_train(spikes)
-print('Spike train:', spike_train)
+# print the first 20 spikes in the binary spike train
+print('Spike train:', spike_train[:20])
 
 ###################################################################################################
 #
-# Similarly, we can also convert binary spike train to spike times in millisecond.
+# Similarly, we can also convert binary spike train to spike times in milliseconds.
 #
 
 ###################################################################################################
 
-# Set a binary spike train
-spike_train = [0,0,0,1,0,1,0,0,1,1,1,0,1,1,1,0,0,0,1,1]
-
-# convert binary spike train to spike times in milliseconds
+# convert a binary spike train to spike times in milliseconds
 spike_times = convert_train_to_times(spike_train)
 
-print('Spike times:', spike_times)
-plt.eventplot(spike_times)
+# plot the spike times
+plot_rasters(spike_times)
 
 ###################################################################################################
 #
-# Finally, we can convert a vector of inter-spike intervals in millisecond to spike times in millisecond.
+# Finally, we can convert a vector of inter-spike intervals in millisecond to spike times in milliseconds.
 #
 
 ###################################################################################################
 
-# set a vector of inter-spikes interval in milliseconds
-isis = [1, 2, 3, 5, 9, 13, 20, 21, 22, 23, 31, 36]
+# convert a vector of inter-spike intervals in milliseconds to spike times in milliseconds
 spike_times = convert_isis_to_spikes(isis, offset=0, add_offset=True)
 
-print('Spike times:',spike_times)
-plt.eventplot(spike_times)
+# plot the spike times
+plot_rasters(spike_times)
 
 ###################################################################################################
