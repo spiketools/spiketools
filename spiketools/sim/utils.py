0eff7ead993087487bb4bb160056de64e4b94e27
claire98han <zh2497@columbia.edu>
2021-11-26 00:36:12 -0500

    added doctest

--- spiketools/sim/utils.py
+++ spiketools/sim/utils.py
@@ -3,6 +3,7 @@
 ###################################################################################################
 ###################################################################################################
 
+
 def refractory(spikes, refractory_time, fs):
     """Apply a refractory period to a simulated spike train.
 
@@ -19,6 +20,15 @@ def refractory(spikes, refractory_time, fs):
     -------
     spikes : 1d array
         Spike train, with refractory period constraint applied.
+
+    Examples
+    --------
+
+    Apply a 0.3 seconds refractory period to a binary spike train with 1000 Hz sampling rate.
+
+    >>> spikes = np.array([0,1,1,1,0,1,1,1,0,1,1,1,1,0,1,1,0,1,0,])
+    >>> refractory(spikes, 0.003, 1000)
+    array([0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0])
     """
 
     ref_len = int(refractory_time * fs)
