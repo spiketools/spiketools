c8fe9e304d3ae7d381fc4c41b29df6d52b7facde
Tom Donoghue <tdonoghue.research@gmail.com>
2022-04-03 11:49:05 -0400

    fix api list, etc

--- spiketools/utils/__init__.py
+++ spiketools/utils/__init__.py
@@ -1,5 +1,4 @@
 """Utility functions."""
 
-from .spikes import restrict_range
 from .utils import set_random_seed
-from .data import get_value_by_time, get_value_by_time_range
+from .data import restrict_range, get_value_by_time, get_value_by_time_range
