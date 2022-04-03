f8c78c86641615722573a5fcdc1530ef3bab0c16
Tom Donoghue <tdonoghue.research@gmail.com>
2022-04-03 01:37:32 -0400

    add bin_circular

---
+++ spiketools/measures/circular.py
@@ -0,0 +1,33 @@
+"""Functions for dealing with circular data."""
+
+import numpy as np
+
+###################################################################################################
+###################################################################################################
+
+def bin_circular(data, bin_width=10):
+    """Bin circular data.
+
+    Parameters
+    ----------
+    data : 1d array
+        Data to bin.
+    bin_width : int, optional, default: 10
+        Width of the bins to use for the histogram.
+
+    Returns
+    -------
+    bin_edges : 1d array
+        Bin edge definitions.
+    counts : 1d array
+        Count values per bin.
+
+    Notes
+    -----
+    This function currently only supports data in degrees.
+    """
+
+    bin_edges = np.arange(0, 370, bin_width)
+    counts, _ = np.histogram(data, bins=bin_edges)
+
+    return bin_edges, counts
