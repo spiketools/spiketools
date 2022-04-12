f8dc8e39d83135ad258749e3e95432c1b3805c48
Tom Donoghue <tdonoghue.research@gmail.com>
2022-04-11 20:17:16 -0400

    update spike trains to work with seconds

--- spiketools/measures/conversions.py
+++ spiketools/measures/conversions.py
@@ -5,13 +5,18 @@ import numpy as np
 ###################################################################################################
 ###################################################################################################
 
-def create_spike_train(spikes):
+def create_spike_train(spikes, fs=1000, length=None):
     """Convert spike times into a binary spike train.
 
     Parameters
     ----------
     spikes : 1d array
-        Spike times, in seconds or milliseconds.
+        Spike times, in seconds.
+    fs : int, optional, default: 1000
+        The sampling rate to use for the computed spike train.
+    length : float, optional
+        The total length of the spike train to create.
+        If not provided, the length is set at the maximum timestamp in the input spike times.
 
     Returns
     -------
@@ -22,22 +27,23 @@ def create_spike_train(spikes):
     --------
     Convert 6 spike times into a corresponding binary spike train
 
-    >>> spikes = [250, 500, 750, 1000, 1250, 1500]
+    >>> spikes = [0.002, 0.250, 0.500, 0.750, 1.000, 1.250, 1.500]
     >>> create_spike_train(spikes)
-    array([0., 0., 0., ..., 0., 0., 1.])
+    array([0, 0, 1, ..., 0, 0, 1])
     """
 
-    spike_train = np.zeros((np.ceil(spikes[-1]) + 1).astype(int))
+    if not length:
+        length = np.max(spikes)
 
-    inds = [int(ind) for ind in spikes if ind <= spike_train.shape[-1]]
-
+    spike_train = np.zeros(int(length * fs) + 1).astype(int)
+    inds = [int(ind * fs) for ind in spikes if ind * fs <= spike_train.shape[-1]]
     spike_train[inds] = 1
 
     return spike_train
 
 
 def convert_train_to_times(train):
-    """Convert a spike train representation into spike times, in milliseconds.
+    """Convert a spike train representation into spike times, in seconds.
 
     Parameters
     ----------
@@ -47,18 +53,19 @@ def convert_train_to_times(train):
     Returns
     -------
     spikes : 1d array
-        Spike times, in milliseconds.
+        Spike times, in seconds.
 
     Examples
     --------
-    Convert a spike train (assumed 1000 Hz sampling rate) into spike times, in milliseconds.
+    Convert a spike train (assumed 1000 Hz sampling rate) into spike times, in seconds.
 
-    >>> spike_train = [0,0,0,1,0,1,0,0,1,1,1,1,0,1]
+    >>> spike_train = [0,0,0,1,0,1,0,0,1,0,1,1,0,1]
     >>> convert_train_to_times(spike_train)
-    array([ 4,  6,  9, 10, 11, 12, 14])
+    array([0.004, 0.006, 0.009, 0.011, 0.012, 0.014])
     """
 
     spikes = np.where(train)[0] + 1
+    spikes = spikes / 1000
 
     return spikes
 
@@ -69,7 +76,7 @@ def convert_isis_to_spikes(isis, offset=0, add_offset=True):
     Parameters
     ----------
     isis : 1d array
-        Distribution of interspike intervals, in milliseconds.
+        Distribution of interspike intervals, in seconds.
     offset : float, optional
         An offset value to add to generated spike times.
     add_offset : bool, optional, default: True
@@ -78,15 +85,15 @@ def convert_isis_to_spikes(isis, offset=0, add_offset=True):
     Returns
     -------
     spikes : 1d array
-        Spike times, in milliseconds.
+        Spike times, in seconds.
 
     Examples
     --------
-    Convert a sequence of 6 inter-spike intervals (ms) to their corresponding spike times (ms).
+    Convert a sequence of 6 inter-spike intervals to their corresponding spike times, in seconds.
 
-    >>> isis = [300, 600, 800, 200, 700]
+    >>> isis = [0.3, 0.6, 0.8, 0.2, 0.7]
     >>> convert_isis_to_spikes(isis, offset=0, add_offset=True)
-    array([   0,  300,  900, 1700, 1900, 2600])
+    array([0. , 0.3, 0.9, 1.7, 1.9, 2.6])
     """
 
     spikes = np.cumsum(isis, axis=-1)
