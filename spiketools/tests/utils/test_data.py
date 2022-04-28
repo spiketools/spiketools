ebeed9100795494edd465e913747d7d4be38bcdd
Tom Donoghue <tdonoghue.research@gmail.com>
2022-04-28 15:35:57 -0400

    generalize context / description of restrict_range

--- spiketools/tests/utils/test_data.py
+++ spiketools/tests/utils/test_data.py
@@ -20,13 +20,13 @@ def test_restrict_range():
 
     data = np.array([0.5, 1., 1.5, 2., 2.5])
 
-    out1 = restrict_range(data, min_time=1.)
+    out1 = restrict_range(data, min_value=1.)
     assert np.array_equal(out1, np.array([1., 1.5, 2., 2.5]))
 
-    out2 = restrict_range(data, max_time=2.)
+    out2 = restrict_range(data, max_value=2.)
     assert np.array_equal(out2, np.array([0.5, 1., 1.5, 2.]))
 
-    out3 = restrict_range(data, min_time=1., max_time=2.)
+    out3 = restrict_range(data, min_value=1., max_value=2.)
     assert np.array_equal(out3, np.array([1., 1.5, 2.]))
 
 def test_get_value_by_time():
