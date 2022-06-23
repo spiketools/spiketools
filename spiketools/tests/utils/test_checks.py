1d41e9b0d81ad68632fda8ce4bb51a2dbe178992
Tom Donoghue <tdonoghue.research@gmail.com>
2022-06-22 20:03:25 -0400

    add check_time_bins

--- spiketools/tests/utils/test_checks.py
+++ spiketools/tests/utils/test_checks.py
@@ -1,5 +1,7 @@
 """Tests for spiketools.utils.checks"""
 
+from pytest import raises, warns
+
 from spiketools.utils.checks import *
 
 ###################################################################################################
@@ -14,3 +16,20 @@ def test_infer_time_unit(tspikes):
     # Check test data in milliseconds
     inferred = infer_time_unit(tspikes * 1000)
     assert inferred == 'milliseconds'
+
+def test_check_time_bins(tspikes):
+
+    # Check precomputed time bins
+    tbins = np.arange(0, 10, 0.5)
+    out = check_time_bins(tbins, tspikes)
+    assert np.array_equal(tbins, out)
+
+    # Check time bins given a time resolution, which should create same bins as precomputed
+    out = check_time_bins(0.5, tspikes, trange=[0, 10])
+    assert np.array_equal(tbins, out)
+
+    # Check error & warning
+    with raises(AssertionError):
+        out = check_time_bins(np.array([1, 2, 1]), tspikes, trange=[0, 5])
+    with warns(UserWarning):
+        out = check_time_bins(0.5, tspikes, trange=[0, 5])
--- spiketools/utils/checks.py
+++ spiketools/utils/checks.py
@@ -1,5 +1,7 @@
 """Spike related checking functions."""
 
+import warnings
+
 import numpy as np
 
 ###################################################################################################
@@ -34,3 +36,41 @@ def infer_time_unit(time_values):
         time_unit = 'milliseconds'
 
     return time_unit
+
+
+def check_time_bins(bins, values, trange=None, check_range=True):
+    """Check a given time bin definition, and define if only given a time resolution.
+
+    Parameters
+    ----------
+    bins : float or 1d array
+        The binning to apply to the spiking data.
+        If float, the length of each bin.
+        If array, precomputed bin definitions.
+    values : 1d array
+        The time values that are to be binned.
+    trange : list of [float, float]
+        Time range, in seconds, to create the binned firing rate across.
+        Only used if `bins` is a float.
+    check_range : True
+        Whether the check the range of the data values against the time bins.
+
+    Returns
+    -------
+    bins : 1d array
+        Time bins.
+    """
+
+    if isinstance(bins, float):
+        trange = [0, np.max(values) + bins] if not trange else trange
+        bins = np.arange(*trange, bins)
+
+    elif isinstance(bins, np.ndarray):
+        # Check that bins are well defined (monotonically increasing)
+        assert np.all(np.diff(bins) > 0), 'Bin definition is ill-formed.'
+
+    # Check that given bin range matches the data values
+    if check_range and (np.min(values) < bins[0] or np.max(values) > bins[-1]):
+        warnings.warn('The data values extend beyond the given time definition.')
+
+    return bins
