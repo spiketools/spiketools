5a021b40ea515bb2c664ff57910b0c89d3c688bd
Tom Donoghue <tdonoghue.research@gmail.com>
2022-06-23 00:08:03 -0400

    add select_from_list

--- spiketools/utils/base.py
+++ spiketools/utils/base.py
@@ -18,3 +18,22 @@ def flatten(lst):
     """
 
     return [item for sublist in lst for item in sublist]
+
+
+def select_from_list(lst, select):
+    """Select elements from a list based on a boolean mask.
+
+    Parameters
+    ----------
+    lst : list
+        A list of values to select from.
+    select : list of bool
+        Indicator for which elements to select.
+
+    Returns
+    -------
+    lst
+        Selected elements from the list.
+    """
+
+    return [el for el, sel in zip(lst, select) if sel]
