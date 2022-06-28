93362e518d0024609b97bca70c146ac21cddc331
Tom Donoghue <tdonoghue.research@gmail.com>
2022-06-28 15:31:07 -0400

    add x & y ticklabels to setabble plt kwargs

--- spiketools/plts/settings.py
+++ spiketools/plts/settings.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 ###################################################################################################
 
 # Define a list of arguments that will be caught and applied with 'set'
-SET_KWARGS = ['title', 'xlim', 'ylim', 'xlabel', 'ylabel', 'xticks', 'yticks']
+SET_KWARGS = ['title', 'xlim', 'ylim', 'xlabel', 'ylabel',
+              'xticks', 'yticks', 'xticklabels', 'yticklabels']
 
 # Collect a list of the default matplotlib color cycle
 DEFAULT_COLORS = plt.rcParams['axes.prop_cycle'].by_key()['color']
