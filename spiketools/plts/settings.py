953bce133af82673886e99f919de2699ec993752
Tom Donoghue <tdonoghue.research@gmail.com>
2022-06-28 10:29:18 -0400

    add x & y ticks to setabble kwargs through decorator

--- spiketools/plts/settings.py
+++ spiketools/plts/settings.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 ###################################################################################################
 
 # Define a list of arguments that will be caught and applied with 'set'
-SET_KWARGS = ['title', 'xlim', 'ylim', 'xlabel', 'ylabel']
+SET_KWARGS = ['title', 'xlim', 'ylim', 'xlabel', 'ylabel', 'xticks', 'yticks']
 
 # Collect a list of the default matplotlib color cycle
 DEFAULT_COLORS = plt.rcParams['axes.prop_cycle'].by_key()['color']
