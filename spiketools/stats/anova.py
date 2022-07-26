a49e9ecfd1fc65b0ed84d00ac8b659360e793654
claire98han <zh2497@columbia.edu>
2022-07-25 22:29:09 -0400

    fix decimal

--- spiketools/stats/anova.py
+++ spiketools/stats/anova.py
@@ -169,7 +169,7 @@ def fit_anova(df, formula, feature=None, return_type='f_val', anova_type=2):
     >>> data = np.array([[1,2,3,7,2], [4,5,6,4,1], [8,9,10, 9, 8]])
     >>> df = create_dataframe_bins(data)
     >>> fit_anova(df, 'fr ~ C(bin)', feature='C(bin)', return_type='f_val', anova_type=1)
-    0.42485549132947986
+    0.4248554913294796
     """
 
     check_param_options(return_type, 'return_type', ['model', 'results', 'f_val'])
