adf8a3e3f5746ca54801b7f8e5c3dfac82e56634
Tom Donoghue <tdonoghue.research@gmail.com>
2022-09-03 20:48:27 -0400

    update inits (remove things)

--- spiketools/measures/__init__.py
+++ spiketools/measures/__init__.py
@@ -1,4 +1,1 @@
-"""Spike related measures."""
-
-from .measures import compute_firing_rate, compute_isis, compute_cv, compute_fano_factor
-from .conversions import convert_times_to_train
+"""Functions for computing measures on various data types / organizations."""
