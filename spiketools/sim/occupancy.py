7a90a42ac678f50bc3180fcc59bd943cc5c4c951
weijia zhang <wz2540@columbia.edu>
2025-09-03 14:40:51 -0400

    sim function names

--- spiketools/sim/occupancy.py
+++ spiketools/sim/occupancy.py
@@ -5,7 +5,7 @@ import numpy as np
 ###################################################################################################
 ###################################################################################################
 
-def single_trial_occ(num_bins, min_occ, max_occ):
+def sim_single_trial_occ(num_bins, min_occ, max_occ):
     """Simulate occupancy for a single trial.
 
     Parameters
@@ -26,7 +26,7 @@ def single_trial_occ(num_bins, min_occ, max_occ):
     return np.random.uniform(min_occ, max_occ, size=num_bins)  
 
 
-def single_uniform_occ(num_bins, scale):
+def sim_single_uniform_occ(num_bins, scale):
     """Simulate uniform occupancy for a single trial.
     Parameters
     ----------
@@ -46,7 +46,7 @@ def single_uniform_occ(num_bins, scale):
 
 def sim_occ_trials(num_trials, num_bins, min_occ, max_occ, use_random=False, uniform_scale=1):
     """Simulate occupancy for multiple trials.
-    
+
     Parameters
     ----------
     num_trials: int
