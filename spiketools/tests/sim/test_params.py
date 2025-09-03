fbe86589593dbc0304babe76d5f60e54f5ec207f
weijia zhang <wz2540@columbia.edu>
2025-09-03 15:56:51 -0400

    tests

--- spiketools/tests/sim/test_params.py
+++ spiketools/tests/sim/test_params.py
@@ -6,7 +6,7 @@ from spiketools.tests.conftest import params
 ###################################################################################################
 ###################################################################################################
 
-def test_upd_height():
+def test_upd_height(params):
     """Test upd_height"""
 
     params_copy = params()
@@ -14,14 +14,15 @@ def test_upd_height():
     assert params_copy['height_mean'] == 10
 
 
-def test_upd_width():
+def test_upd_width(params):
     """Test upd_width"""
 
-    upd_width(params, 10)
+    params_copy = params()
+    upd_width(params_copy, 10)
     assert params['width_mean'] == 10
 
 
-def test_upd_noise():
+def test_upd_noise(params):
     """Test upd_noise"""
 
     params_copy = params()
@@ -29,7 +30,7 @@ def test_upd_noise():
     assert params_copy['noise_std'] == 2
 
 
-def test_upd_placeloc():
+def test_upd_placeloc(params):
     """Test upd_placeloc"""
     
     params_copy = params()
@@ -37,7 +38,7 @@ def test_upd_placeloc():
     assert params_copy['place_loc_std'] == 5
 
 
-def test_upd_skewness():
+def test_upd_skewness(params):
     """Test upd_skewness""" 
 
     params_copy = params()
@@ -45,7 +46,7 @@ def test_upd_skewness():
     assert params_copy['skewness_mean'] == 2
 
 
-def test_upd_presence_ratio():
+def test_upd_presence_ratio(params):
     """Test upd_presence_ratio"""
 
     params_copy = params()
@@ -53,7 +54,7 @@ def test_upd_presence_ratio():
     assert params_copy['presence_ratio'] == 0.5
 
 
-def test_upd_base():
+def test_upd_base(params):
     """Test upd_base"""
 
     params_copy = params()
@@ -61,7 +62,7 @@ def test_upd_base():
     assert params_copy['base_mean'] == 2
 
 
-def test_upd_trials():
+def test_upd_trials(params):
     """Test upd_trials"""
 
     params_copy = params()
@@ -69,30 +70,30 @@ def test_upd_trials():
     assert params_copy['n_trials'] == 20
 
 
-def test_upd_npeaks():
+def test_upd_npeaks(params):
     """Test upd_npeaks"""
-    params = {'n_height_mean' : [5], 'n_width_mean' : [5], 'n_place_locs_mean' : [5], 
+    params_test = {'n_height_mean' : [5], 'n_width_mean' : [5], 'n_place_locs_mean' : [5], 
     'n_place_loc_std' : [5], 'n_height_std' : [5], 'n_width_std' : [5], 
-            'n_trials' : 10, 'n_bins' : 10, 'n_peaks' : 1}
-    upd_npeaks(params, 2)
-    assert params['n_peaks'] == 2
-    assert len(params['n_height_mean']) == 2
-    assert len(params['n_width_mean']) == 2
-    assert len(params['n_place_locs_mean']) == 2
-    assert len(params['n_place_loc_std']) == 2
-    assert len(params['n_height_std']) == 2
-    assert len(params['n_width_std']) == 2
+    'n_skewness_mean' : [5], 'n_skewness_std' : [1], 'n_trials' : 10, 'n_bins' : 10, 'n_peaks' : 1}
+    upd_npeaks(params_test, 2)
+    assert params_test['n_peaks'] == 2
+    assert len(params_test['n_height_mean']) == 2
+    assert len(params_test['n_width_mean']) == 2
+    assert len(params_test['n_place_locs_mean']) == 2
+    assert len(params_test['n_place_loc_std']) == 2
+    assert len(params_test['n_height_std']) == 2
+    assert len(params_test['n_width_std']) == 2
 
 
 def test_upd_skew_npeaks():
     """Test upd_skew_npeaks"""
-    params = {'n_height_mean' : [5], 'n_width_mean' : [5], 'n_place_locs_mean' : [5], 
+    params_test = {'n_height_mean' : [5], 'n_width_mean' : [5], 'n_place_locs_mean' : [5], 
     'n_place_loc_std' : [5], 'n_height_std' : [5], 'n_width_std' : [5], 
     'n_skewness_mean' : [5], 'n_skewness_std' : [1], 'n_trials' : 10, 'n_bins' : 10, 'n_peaks' : 1}
-    upd_skew_npeaks(params, 3)
-    assert params['n_peaks'] == 3
-    assert len(params['n_height_mean']) == 3
-    assert len(params['n_width_mean']) == 3 
+    upd_skew_npeaks(params_test, 3)
+    assert params_test['n_peaks'] == 3
+    assert len(params_test['n_height_mean']) == 3
+    assert len(params_test['n_width_mean']) == 3 
     assert len(params['n_place_locs_mean']) == 3
     assert len(params['n_place_loc_std']) == 3
     assert len(params['n_height_std']) == 3
@@ -101,25 +102,23 @@ def test_upd_skew_npeaks():
     assert len(params['n_skewness_std']) == 3
 
 
-def test_update_vals():
+def test_update_vals(params):
     """Test update_vals"""
-    params = {'height_mean' : 5, 'width_mean' : 5, 'noise_std' : 1,
-     'place_loc_std' : 1, 'skewness_mean' : 1, 'presence_ratio' : 1, 'base_mean' : 1,
-    'n_trials' : 10}
+    params_copy = params()
     # Need to iterate through generator returned by update_vals
-    for updated_params in update_vals(params, [10], upd_height):
+    for updated_params in update_vals(params_copy, [10], upd_height):
         pass
-    for updated_params in update_vals(params, [10], upd_width):
+    for updated_params in update_vals(params_copy, [10], upd_width):
         pass
-    for updated_params in update_vals(params, [2], upd_noise):
+    for updated_params in update_vals(params_copy, [2], upd_noise):
         pass
-    for updated_params in update_vals(params, [5], upd_placeloc):
+    for updated_params in update_vals(params_copy, [5], upd_placeloc):
         pass
-    for updated_params in update_vals(params, [2], upd_skewness):
+    for updated_params in update_vals(params_copy, [2], upd_skewness):
         pass
-    for updated_params in update_vals(params, [0.5], upd_presence_ratio):
+    for updated_params in update_vals(params_copy, [0.5], upd_presence_ratio):
         pass
-    for updated_params in update_vals(params, [20], upd_trials):
+    for updated_params in update_vals(params_copy, [20], upd_trials):
         pass
     assert updated_params['height_mean'] == 10
     assert updated_params['width_mean'] == 10
@@ -130,7 +129,7 @@ def test_update_vals():
     assert updated_params['n_trials'] == 20
 
 
-def test_update_paired_vals():
+def test_update_paired_vals(params):
     """Test update_paired_vals"""
     params_copy = params()
     for updated_params in update_paired_vals(params_copy, [10], [10], upd_height, upd_width):
