59072f3e6e6719e09b72145b82cd28a70b3158b3
weijia zhang <wz2540@columbia.edu>
2025-09-03 16:57:16 -0400

    tests

--- spiketools/tests/sim/test_placecells.py
+++ spiketools/tests/sim/test_placecells.py
@@ -15,7 +15,7 @@ def test_sim_neuron_placefield(t_place_sim_params):
     cell_place_bins = sim_neuron_placefield(param_gen)
 
     assert len(cell_place_bins) == 10
-    assert (cell_place_bins[0].shape) == (20, 100)
+    assert (cell_place_bins[0].shape) == (10, 100)
 
 
 def test_sim_neuron_skew_placefield(t_place_sim_skew_params):
@@ -25,26 +25,20 @@ def test_sim_neuron_skew_placefield(t_place_sim_skew_params):
     cell_place_bins = sim_neuron_skew_placefield(param_gen)
 
     assert len(cell_place_bins) == 10
-    assert (cell_place_bins[0].shape) == (20, 100)
+    assert (cell_place_bins[0].shape) == (10, 100)
 
 
 def test_sim_neuron_multi_placefield(t_place_sim_params_npeaks):
     """Test sim_neuron_multi_placefield"""
     params_copy = copy.deepcopy(t_place_sim_params_npeaks)
-    param_gen = update_vals(params_copy, np.linspace(1, 10, 10), upd_npeaks)
-    cell_place_bins = sim_neuron_multi_placefield(param_gen)
-
-    assert len(cell_place_bins) == 10
-    assert (cell_place_bins[0].shape) == (20, 100)
-
-
-    param_gen = update_vals(params, [1,2,3], upd_npeaks)
+    param_gen = update_vals(params_copy, [1,2,3], upd_npeaks)
     cell_place_bins = sim_neuron_multi_placefield(param_gen)
 
     assert len(cell_place_bins) == 3
     assert (cell_place_bins[0].shape) == (10, 100)
     assert (cell_place_bins[1].shape) == (10, 100)
     assert (cell_place_bins[2].shape) == (10, 100)
+
 
 
 def test_sim_neuron_multi_skew_placefield(t_place_sim_params_npeaks_skew):
