9af3815f32f091b5256dcabedeb74620104123b1
weijia zhang <wz2540@columbia.edu>
2025-09-03 14:50:21 -0400

    sim function names

--- spiketools/tests/sim/test_occupancy.py
+++ spiketools/tests/sim/test_occupancy.py
@@ -1,32 +1,33 @@
 """Tests for spiketools.sim.occupancy"""
 
-from spiketools.sim.occupancy import sim_occ_trials, single_trial_occ, single_uniform_occ
+from spiketools.sim.occupancy import (sim_single_trial_occupancy, sim_uniform_occupancy,
+                                      sim_occupancy_trials)
 import numpy as np
 
 ###################################################################################################
 ###################################################################################################
 
-def test_single_trial_occ():
+def test_single_trial_occupancy():
 
     num_bins = 100
     min_occ = 0.0
     max_occ = 1.0
 
-    trial_occ = single_trial_occ(num_bins, min_occ, max_occ)
+    trial_occ = sim_single_trial_occupancy(num_bins, min_occ, max_occ)
     assert isinstance(trial_occ, np.ndarray)
     assert trial_occ.shape == (num_bins,)
 
 
-def test_single_uniform_occ():
+def test_single_uniform_occupancy():
 
     num_bins = 100
     scale = 1.0
-    uniform_occ = single_uniform_occ(num_bins, scale)
+    uniform_occ = sim_uniform_occupancy(num_bins, scale)
     assert isinstance(uniform_occ, np.ndarray)
     assert uniform_occ.shape == (num_bins,)
 
 
-def test_sim_occ_trials():
+def test_sim_occupancy_trials():
 
     num_trials = 10
     num_bins = 100
@@ -35,7 +36,7 @@ def test_sim_occ_trials():
     use_random = False
     uniform_scale = 1.0
 
-    all_trials_occ, avg_occ = sim_occ_trials(num_trials, num_bins, min_occ, max_occ,
+    all_trials_occ, avg_occ = sim_occupancy_trials(num_trials, num_bins, min_occ, max_occ,
      use_random, uniform_scale)
     assert isinstance(all_trials_occ, np.ndarray)
     assert isinstance(avg_occ, np.ndarray)
@@ -43,7 +44,7 @@ def test_sim_occ_trials():
     assert avg_occ.shape == (num_bins,)
 
     use_random = True
-    all_trials_occ_random, avg_occ_random = sim_occ_trials(num_trials, num_bins, 
+    all_trials_occ_random, avg_occ_random = sim_occupancy_trials(num_trials, num_bins, 
                                     min_occ, max_occ, use_random, uniform_scale)
     assert isinstance(all_trials_occ_random, np.ndarray)
     assert isinstance(avg_occ_random, np.ndarray)
