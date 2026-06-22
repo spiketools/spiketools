d6eb3aa23aeb5c9542c8031e978e653e4f479078
Tom Donoghue <tdonoghue.research@gmail.com>
2026-06-22 10:59:03 +0100

    update default color sequence from mpl to hex

--- spiketools/plts/settings.py
+++ spiketools/plts/settings.py
@@ -1,6 +1,7 @@
 """Default settings for plots."""
 
 import matplotlib.pyplot as plt
+from matplotlib.colors import to_hex
 
 ###################################################################################################
 ###################################################################################################
@@ -12,8 +13,10 @@ SET_KWARGS = ['title', 'xlim', 'ylim', 'xlabel', 'ylabel',
 # Define a list of other arguments to be caught
 OTHER_KWARGS = ['legend']
 
-# Collect a list of the default matplotlib color cycle
-DEFAULT_COLORS = plt.rcParams['axes.prop_cycle'].by_key()['color']
+# Define list of default plot colors, making sure colors are hex, for downstream consistency
+#   Hex encoding changed in mpl: https://github.com/matplotlib/matplotlib/issues/29915
+DEFAULT_COLORS = [to_hex(col) for col in plt.rcParams['axes.prop_cycle'].by_key()['color']] \
+    if plt else None
 
 # Define default settings for plotting text
 TEXT_SETTINGS = {'fontdict' : {'fontsize' : 14}, 'ha' : 'center', 'va' : 'center'}
